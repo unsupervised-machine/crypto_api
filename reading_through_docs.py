ac4fdbee9e6636abfc7ea5283c37432738eef1b7
hunter <taran.s.lau@gmail.com>
2024-04-13 10:01:54 -0500

    added functionality to generate coin categories, market data by category or coin ID

--- reading_through_docs.py
+++ reading_through_docs.py
@@ -45,6 +45,8 @@ def save_json(dictionary, filename):
 class ENDPOINTS():
     def __init__(self):
         self.coin_list = None
+        self.currency_list = None
+        self.coin_categories = None
         self.key = None
         with open('my_api_key') as f:
             self.key = f.read().strip()
@@ -93,6 +95,7 @@ class ENDPOINTS():
             precision='full'
     ):
         """
+        EndPOINT function for getting price of coins.
         April 12th 2024: Looks like optional query endpoints do not add additional return values, only return prices.
 
         :param coin_api_ids: list of strings
@@ -110,6 +113,7 @@ class ENDPOINTS():
         # Test if below adds functionality, else remove.
         if len(coin_api_ids) > 1:
             for coin_api_id in coin_api_ids[1:]:
+                # TODO change from %2C%20 to %2C
                 url += f"%2C%20{coin_api_id}"
         url += f"&vs_currencies={currency}"
         if include_market_cap:
@@ -133,28 +137,123 @@ class ENDPOINTS():
         response_dict = response.json()
         return response.text
 
+    def fetch_currencies_list(self):
+        url = "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+        headers = {
+            "accept": "application/json",
+            "x-cg-api-key": self.key
+        }
+        response = requests.get(url, headers=headers)
+        response_dict = response.json()
+        return response_dict
+
+    def set_currencies_list(self):
+        self.currency_list = self.fetch_currencies_list()
+        return None
+
+    def fetch_market_data(self, coin_api_ids=['bitcoin', 'ethereum', 'litecoin'], currency='btc', category=None):
+        """
+
+        :param coin_api_ids: list of strings
+        :param currency: string
+        :param category: string, default to None. If not set to none will prioritize category over coin_api_ids parameter.
+        :return:
+        """
+
+        url = f"https://api.coingecko.com/api/v3/coins/markets?vs_currency={currency}&ids="
+        url += coin_api_ids[0]
+        if len(coin_api_ids) > 1:
+            for coin_api_id in coin_api_ids[1:]:
+                url += f"%2C{coin_api_id}"
+
+        if category:
+            url += f"&category={category}"
+
+        headers = {
+            "accept": "application/json",
+            "x-cg-api-key": self.key
+        }
+        response = requests.get(url, headers=headers)
+        response_dict = response.json()
+        return response_dict
+
+    def fetch_coin_categories(self):
+        url = "https://api.coingecko.com/api/v3/coins/categories/list"
+        headers = {
+            "accept": "application/json",
+            "x-cg-api-key": self.key
+        }
+        response = requests.get(url, headers=headers)
+        response_dict = response.json()
+        return response_dict
+
+    def set_coin_categories(self):
+        self.coin_categories = self.fetch_coin_categories()
+
+
+
+
+
+# Testing Functions
 
 def ping_test(endpoints_obj):
-    endpoints.ping()
+    endpoints_obj.ping()
+
 
 def coin_list_test(endpoints_obj):
-    endpoints.set_coins_list()
-    print(endpoints.coin_list)
+    endpoints_obj.set_coins_list()
+    print(endpoints_obj.coin_list)
+
 
 def price_examples_test(endpoints_obj):
-    bitcoin_example = endpoints.get_coin_price(['bitcoin'], 'usd', True, True, True, True)
+    bitcoin_example = endpoints_obj.get_coin_price(['bitcoin'], 'usd', True, True, True, True)
     print(bitcoin_example)
-    ethereum_example = endpoints.get_coin_price(['ethereum'], 'usd', True, True, True, True)
+    ethereum_example = endpoints_obj.get_coin_price(['ethereum'], 'usd', True, True, True, True)
     print(ethereum_example)
-    two_coins_example = endpoints.get_coin_price(['bitcoin', 'ethereum'], 'usd', True, True, True, True)
+    two_coins_example = endpoints_obj.get_coin_price(['bitcoin', 'ethereum'], 'usd', True, True, True, True)
     print(two_coins_example)
+
+
+def currency_list_test(endpoints_obj):
+    currencies_list = endpoints_obj.fetch_currencies_list()
+    print(currencies_list)
+
+
+def market_data_test_ids(endpoints_obj):
+    market_data = endpoints_obj.fetch_market_data(
+        coin_api_ids=['bitcoin', 'ethereum', 'litecoin'],
+        currency='btc',
+        category=None
+    )
+    print(market_data)
+    save_json(market_data, 'market_data_example.json')
+    return
+
+
+def market_data_test_categories(endpoints_obj):
+    market_data = endpoints_obj.fetch_market_data(
+        currency='btc',
+        category="layer-1"
+    )
+    print(market_data)
+    save_json(market_data, 'market_data_category.json')
+    return
+
+def coin_categories_test(endpoints_obj):
+    categories = endpoints_obj.set_coin_categories()
+    print(endpoints_obj.coin_categories)
+
 
 
 if __name__ == '__main__':
     endpoints = ENDPOINTS()
     # ping_test(endpoints)
     # coin_list_test(endpoints)
-    price_examples_test(endpoints)
+    # price_examples_test(endpoints)
+    # currency_list_test(endpoints)
+    # market_data_test_ids(endpoints)
+    # market_data_test_categories(endpoints)
+    coin_categories_test(endpoints)
 
 
 # if __name__ == '__main__':
