88d05849195980dcb1986eca08757c20ad8a83c5
unsupervised-machine <taran.s.lau@gmail.com>
2024-07-19 12:00:03 -0700

    working on adding sign up button

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,16 @@ import os
 import json
 import requests
 
+
+# # Configures the default settings of the page.
+st.set_page_config(page_title="crypto_api", layout="wide")
 # Streamlit login form
 st.title("Login")
 
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+username = st.sidebar.text_input("Username")
+password = st.sidebar.text_input("Password", type="password")
 
-if st.button("Login"):
+if st.sidebar.button("Login"):
     response = requests.post("http://localhost:8000/token", data={"username": username, "password": password})
     if response.status_code == 200:
         token = response.json().get("access_token")
@@ -21,6 +24,8 @@ if st.button("Login"):
         st.error("Login failed")
 
 if "token" in st.session_state:
+    st.markdown("### Logged in")
+
     headers = {"Authorization": f"Bearer {st.session_state.token}"}
     response = requests.get("http://localhost:8000/users/me/", headers=headers)
 
@@ -30,15 +35,24 @@ if "token" in st.session_state:
     else:
         st.error("Unauthorized access")
 
+    if st.sidebar.button("Logout"):
+        del st.session_state.token
+        st.success("Logged out successfully!")
+
+else:
+    st.markdown("### Not logged in")
+
+    st.sidebar.button("Sign up")
+
 
 # -- UNCOMMENT EVERYTHING BELOW WHEN DONE TESTING -- ##
-# MOCK_DATA = 'data/MOCK_DATA.json'
+MOCK_DATA = 'data/MOCK_DATA.json'
 #
 # # Configures the default settings of the page.
 # st.set_page_config(page_title="crypto_api", layout="wide")
 #
 #
-# st.title('My Cryptocurrency app')
+st.title('My Cryptocurrency app')
 #
 #
 #
@@ -60,80 +74,80 @@ if "token" in st.session_state:
 # # st.write("App path: ", app_location)
 #
 #
-# with open(MOCK_DATA, 'r') as file:
-#     data_dict = json.load(file)
-#
-# df = pd.DataFrame(data_dict)
-#
-#
-# def dict_to_list(d):
-#     """
-#     Helper function to convert column of dicts to column lists
-#     """
-#     return d['price']
-#
-#
-# # Want to convert the sparklines from dicts to lists
-# df['sparkline_in_7d'] = df['sparkline_in_7d'].apply(dict_to_list)
-#
-# # Add column of false values for favorite checkboxes
-# df.insert(0, 'favorite', False)
-#
-# # data_dict = data_dict.iloc[:, 1:] # Drop db _id column
-# columns_to_keep = [
-#     'favorite',
-#     'market_cap_rank',
-#     'image',
-#     'id',
-#     'current_price',
-#     'price_change_percentage_24h',
-#     'market_cap',
-#     'sparkline_in_7d',
-# ]
-#
-# # Only keep columns we care about
-# df = df[columns_to_keep]
-#
-# # Only allow edits for favorite column, used int 'disabled' for data_editor
-# columns_to_edit = ['favorite']
-# columns_all = df.columns.to_list()
-# columns_not_to_edit = [col for col in columns_all if col not in columns_to_edit]
-#
-#
-# st.data_editor(
-#     data=df,
-#     width=None,
-#     use_container_width=False,
-#     height=2000,
-#     disabled=columns_not_to_edit,
-#     column_config={
-#         "favorite": st.column_config.CheckboxColumn(
-#             "Favorite?",
-#             help="Select your **favorite** currencies.",
-#             default=False
-#         ),
-#         "image": st.column_config.ImageColumn(
-#             "Icon", help="Icons for currencies"
-#         ),
-#         "current_price": st.column_config.NumberColumn(
-#             label="current_price",
-#             format='$%g',
-#             help="USD",
-#         ),
-#         "price_change_percentage_24h": st.column_config.NumberColumn(
-#             label="price_change_percentage_24h",
-#             format="%.2f%%",
-#         ),
-#         "market_cap": st.column_config.NumberColumn(
-#             label="market_cap",
-#             format="$%g",
-#             help="USD",
-#         ),
-#         "sparkline_in_7d": st.column_config.LineChartColumn(
-#             "Last 7 days",
-#             help="Line chart for the last 7 days",
-#         ),
-#
-#     },
-#     hide_index=True
-# )
+with open(MOCK_DATA, 'r') as file:
+    data_dict = json.load(file)
+
+df = pd.DataFrame(data_dict)
+
+
+def dict_to_list(d):
+    """
+    Helper function to convert column of dicts to column lists
+    """
+    return d['price']
+
+
+# Want to convert the sparklines from dicts to lists
+df['sparkline_in_7d'] = df['sparkline_in_7d'].apply(dict_to_list)
+
+# Add column of false values for favorite checkboxes
+df.insert(0, 'favorite', False)
+
+# data_dict = data_dict.iloc[:, 1:] # Drop db _id column
+columns_to_keep = [
+    'favorite',
+    'market_cap_rank',
+    'image',
+    'id',
+    'current_price',
+    'price_change_percentage_24h',
+    'market_cap',
+    'sparkline_in_7d',
+]
+
+# Only keep columns we care about
+df = df[columns_to_keep]
+
+# Only allow edits for favorite column, used int 'disabled' for data_editor
+columns_to_edit = ['favorite']
+columns_all = df.columns.to_list()
+columns_not_to_edit = [col for col in columns_all if col not in columns_to_edit]
+
+
+st.data_editor(
+    data=df,
+    width=None,
+    use_container_width=False,
+    height=2000,
+    disabled=columns_not_to_edit,
+    column_config={
+        "favorite": st.column_config.CheckboxColumn(
+            "Favorite?",
+            help="Select your **favorite** currencies.",
+            default=False
+        ),
+        "image": st.column_config.ImageColumn(
+            "Icon", help="Icons for currencies"
+        ),
+        "current_price": st.column_config.NumberColumn(
+            label="current_price",
+            format='$%g',
+            help="USD",
+        ),
+        "price_change_percentage_24h": st.column_config.NumberColumn(
+            label="price_change_percentage_24h",
+            format="%.2f%%",
+        ),
+        "market_cap": st.column_config.NumberColumn(
+            label="market_cap",
+            format="$%g",
+            help="USD",
+        ),
+        "sparkline_in_7d": st.column_config.LineChartColumn(
+            "Last 7 days",
+            help="Line chart for the last 7 days",
+        ),
+
+    },
+    hide_index=True
+)
